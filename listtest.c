942043222d4ed1c7cec7f4632f2e64763d037fa3
Qiao Zhang <qiao@cs.washington.edu>
2014-10-01 02:05:03 +0800

    adds tests for list_find and list_remove

--- listtest.c
+++ listtest.c
@@ -4,6 +4,15 @@
 
 #define TESTSIZE 133
 
+static
+int
+int_comparator(void* left, void* right)
+{
+    int l = *(int*)left;
+    int r = *(int*)right;
+    return l-r;
+}
+
 int
 list_test()
 {
@@ -15,16 +24,40 @@ list_test()
     assert(list_getsize(newlist) == 0);
     assert(list_isempty(newlist));
 
-    int i;
+    int i, found, removed;
     int* elem;
     /* push back TESTSIZE number of elements */
     for (i = 0; i < TESTSIZE; ++i) {
         elem = (int*)malloc(sizeof(int));
         *elem = i;
         list_push_back(newlist, (void*) elem);
+        /* find the element added */
+        found = *(int*)list_find(newlist, (void*) elem, &int_comparator);
+        assert(found == i);
     }
     assert(list_getsize(newlist) == TESTSIZE);
     assert(!list_isempty(newlist));
+
+    /* remove TESTSIZE number of elements */
+    for (i = 0; i < TESTSIZE; ++i) {
+        elem = (int*)malloc(sizeof(int));
+        *elem = i;
+        /* remove the element added */
+        removed = *(int*)list_remove(newlist, (void*) elem, &int_comparator);
+        assert(removed == i);
+    }
+    assert(list_getsize(newlist) == 0);
+    assert(list_isempty(newlist));
+
+    /* push back TESTSIZE number of elements */
+    for (i = 0; i < TESTSIZE; ++i) {
+        elem = (int*)malloc(sizeof(int));
+        *elem = i;
+        list_push_back(newlist, (void*) elem);
+        /* find the element added */
+        found = *(int*)list_find(newlist, (void*) elem, &int_comparator);
+        assert(found == i);
+    }
 
     /* pop front TESTSIZE number of elements */
     for (i = 0; i < TESTSIZE; ++i) {
@@ -36,14 +69,40 @@ list_test()
     assert(list_getsize(newlist) == 0);
     assert(list_isempty(newlist));
 
+    /* REPEAT to check if list is reusable */
+
     /* push back TESTSIZE number of elements */
     for (i = 0; i < TESTSIZE; ++i) {
         elem = (int*)malloc(sizeof(int));
         *elem = i;
         list_push_back(newlist, (void*) elem);
+        /* find the element added */
+        found = *(int*)list_find(newlist, (void*) elem, &int_comparator);
+        assert(found == i);
     }
     assert(list_getsize(newlist) == TESTSIZE);
     assert(!list_isempty(newlist));
+
+    /* remove TESTSIZE number of elements */
+    for (i = 0; i < TESTSIZE; ++i) {
+        elem = (int*)malloc(sizeof(int));
+        *elem = i;
+        /* remove the element added */
+        removed = *(int*)list_remove(newlist, (void*) elem, &int_comparator);
+        assert(removed == i);
+    }
+    assert(list_getsize(newlist) == 0);
+    assert(list_isempty(newlist));
+
+    /* push back TESTSIZE number of elements */
+    for (i = 0; i < TESTSIZE; ++i) {
+        elem = (int*)malloc(sizeof(int));
+        *elem = i;
+        list_push_back(newlist, (void*) elem);
+        /* find the element added */
+        found = *(int*)list_find(newlist, (void*) elem, &int_comparator);
+        assert(found == i);
+    }
 
     /* pop front TESTSIZE number of elements */
     for (i = 0; i < TESTSIZE; ++i) {
